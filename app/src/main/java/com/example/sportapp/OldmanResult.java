8de43de1567104084f194d1ed000cf043c1ece69
jameswang0 <jason980160@gmail.com>
2020-05-08 10:20:39 +0800

    set chatbotUI

--- app/src/main/java/com/example/sportapp/OldmanResult.java
+++ app/src/main/java/com/example/sportapp/OldmanResult.java
@@ -9,19 +9,10 @@ import android.widget.Button;
 
 public class OldmanResult extends AppCompatActivity {
 
-    private Button return_btn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oldman_result);
 
-        return_btn = findViewById(R.id.button);
-        return_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(OldmanResult.this, MainActivity.class));
-            }
-        });
     }
 }
